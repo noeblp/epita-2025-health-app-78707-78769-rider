5201ffec061359c468e9b284ea391da51919bc7b
noeblp <149522348+noeblp@users.noreply.github.com>
2025-03-18 11:26:01 +0000

    ra

--- HealthApp.MVC/Controllers/CalendarController.cs
+++ HealthApp.MVC/Controllers/CalendarController.cs
@@ -1,8 +1,12 @@
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
-using System.Globalization;
+using System.Data.SqlClient;
+using System.Linq;
 using hospital.Models;
+using hospital.Modif_data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Data.Sqlite;
 using Calendar = hospital.Models.Calendar;
 
 namespace hospital.Controllers
@@ -21,11 +25,24 @@ namespace hospital.Controllers
             if (firstMonday > firstDayOfMonth) firstMonday = firstMonday.AddDays(-7);
 
             var startOfWeek = firstMonday.AddDays((currentWeek - 1) * 7);
+            HttpContext.Session.GetString("user_id");
+            
+            
+            
+            
+            int patientId = 8; // Exemple d'ID patient
+            using (var connection = ModifUser.ConnectToDatabase())
+            {
+                GetPatientEvents(connection,"8");
+            }
+            
 
+            
+            
             var events = new List<Calendar>
             {
-                new Calendar { Title = "Réunion", Date = new DateTime(currentYear, currentMonth, 13) },
-                //new Calendar { Title = "Conférence", Date = new DateTime(currentYear, currentMonth, 15) }
+                //new Calendar { Title = "Réunion", Date = new DateTime(currentYear, currentMonth, 18, 10, 30, 0) },
+                //new Calendar { Title = "Conférence", Date = new DateTime(currentYear, currentMonth, 20, 14, 0, 0) }
             };
 
             ViewBag.CurrentYear = currentYear;
@@ -42,5 +59,30 @@ namespace hospital.Controllers
             DateTime firstDay = new DateTime(date.Year, date.Month, 1);
             return (date.Day + (int)firstDay.DayOfWeek - 1) / 7 + 1;
         }
+
+
+        static void GetPatientEvents(SqliteConnection connection,string patientId)
+        {
+            var query = "SELECT date FROM appointment WHERE patient_id = @patient_id";
+
+            using SqliteCommand command = new SqliteCommand(query, connection);
+            command.Parameters.AddWithValue("@patient_id", 8);
+
+            connection.Open();
+            var result = command.ExecuteScalar();
+            if (result != null)
+            {
+                Console.WriteLine("ee" + result);
+            }
+            else
+            {
+                Console.WriteLine("vide");
+            }
+
+            connection.Close();
+
+            
+        }
+
     }
 }
