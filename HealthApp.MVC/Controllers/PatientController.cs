8b938ea621e69050f5e35ad22b0375e7c6d84f76
ArthosGamos <vendraminiarthur@gmail.com>
2025-03-18 15:07:42 +0000

    SearchPartiallydone
    manque l'affichage du nom des docteurs

--- HealthApp.MVC/Controllers/PatientController.cs
+++ HealthApp.MVC/Controllers/PatientController.cs
@@ -1,6 +1,7 @@
 using hospital.Modif_data;
 using hospital.Views.Patient;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.Sqlite;
 using Microsoft.EntityFrameworkCore;
 
 namespace hospital.Controllers;
@@ -9,6 +10,45 @@ public class PatientController:Controller
 {
     public IActionResult Manage()
     {
+        return View();
+    }
+    
+    static List<string> GetDoctorList(SqliteConnection connection)
+    {
+        var query = "SELECT doctor_last_name FROM doctors";
+
+        using SqliteCommand command = new SqliteCommand(query, connection);
+        
+
+        connection.Open();
+        using SqliteDataReader reader = command.ExecuteReader();
+
+        List<string> last_names = new List<string>();
+        while (reader.Read())
+        {
+            last_names.Add(reader["doctor_last_name"].ToString());
+        }
+
+        connection.Close();
+        return last_names;
+    }
+
+
+
+    public IActionResult Search(string doctorName, string specialty)
+    {
+        List<string> doctorList = new List<string>();
+
+        using (var connection = ModifUser.ConnectToDatabase())
+        {
+            if (!string.IsNullOrEmpty(doctorName))
+            {
+                doctorList = GetDoctorList(connection); 
+            }
+           
+        }
+
+        ViewBag.DoctorList = doctorList;
         return View();
     }
 
@@ -22,4 +62,5 @@ public class PatientController:Controller
         }
        
     }
+    
 }
