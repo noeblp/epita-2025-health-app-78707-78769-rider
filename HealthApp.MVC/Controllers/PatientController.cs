a964e5275ab8203d55f46943b910fc7f230dd592
ArthosGamos <vendraminiarthur@gmail.com>
2025-03-19 14:53:10 +0000

    DoctorResearch
    working with last names only, Specialties will be implmented later

--- HealthApp.MVC/Controllers/PatientController.cs
+++ HealthApp.MVC/Controllers/PatientController.cs
@@ -14,44 +14,65 @@ public class PatientController:Controller
         return View();
     }
     
-    static List<string> GetDoctorList(SqliteConnection connection)
+    private static List<string> GetDoctorList(SqliteConnection connection, string doctorName, string specialty)
     {
-        var query = "SELECT doctor_last_name FROM doctors";
+        var query = "SELECT doctor_last_name FROM doctors WHERE 1=1";
+    
+        if (!string.IsNullOrEmpty(doctorName))
+        {
+            query += " AND doctor_last_name LIKE @doctorName";
+        }
+        if (!string.IsNullOrEmpty(specialty))
+        {
+            query += " AND specialty = @specialty";
+        }
 
         using SqliteCommand command = new SqliteCommand(query, connection);
-        
+    
+        if (!string.IsNullOrEmpty(doctorName))
+        {
+            command.Parameters.AddWithValue("@doctorName", "%" + doctorName + "%"); // Search for partial match
+        }
+        if (!string.IsNullOrEmpty(specialty))
+        {
+            command.Parameters.AddWithValue("@specialty", specialty);
+        }
 
         connection.Open();
         using SqliteDataReader reader = command.ExecuteReader();
 
-        List<string> last_names = new List<string>();
+        List<string> lastNames = new List<string>();
         while (reader.Read())
         {
-            last_names.Add(reader["doctor_last_name"].ToString());
+            lastNames.Add(reader["doctor_last_name"].ToString());
         }
 
         connection.Close();
-        return last_names;
+        return lastNames;
     }
-
-
-
-    public IActionResult Search(string doctorName, string specialty)
+    
+    
+    [HttpGet]
+    public IActionResult Search(string doctorName = null, string specialty = null)
     {
         List<string> doctorList = new List<string>();
 
-        using (var connection = ModifUser.ConnectToDatabase())
+        // Only query the database if either doctorName or specialty is provided
+        if (!string.IsNullOrEmpty(doctorName) || !string.IsNullOrEmpty(specialty))
         {
-            if (!string.IsNullOrEmpty(doctorName))
+            using (var connection = ModifUser.ConnectToDatabase())
             {
-                doctorList = GetDoctorList(connection); 
+                doctorList = GetDoctorList(connection, doctorName, specialty);
             }
-           
+
+            ViewBag.DoctorList = doctorList;
+            return View("DoctorSearch"); // Render the DoctorSearch view with results
         }
 
-        ViewBag.DoctorList = doctorList;
+        // If no search criteria are provided, render the Search view
         return View();
     }
+
 
 
     public void Update(string firstname, string lastname, string email, string password)
