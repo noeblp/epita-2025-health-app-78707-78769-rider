9dd792eba67579da680918dcd81901390bb91a65
noeblp <149522348+noeblp@users.noreply.github.com>
2025-03-23 21:07:55 +0100

    re

---
+++ HealthApp.MVC/Controllers/AppointmentController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace hospital.Controllers;
+
+public class AppointmentController:Controller
+{
+    public IActionResult BookAppo()
+    {
+        return View();
+    }
+}
